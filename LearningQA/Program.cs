6e14647b2e61677ed9d6cd0455bbda09626b0d3e
aline.oliveira <aline.oliveira@sensedia.com>
2021-06-04 13:27:13 -0300

    Desafio LearningQA

---
+++ LearningQA/Program.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using Xunit;
+
+
+namespace LearningQA
+{
+    class Program
+    {
+
+        static void Main(String[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            driver.Manage().Window.Maximize();
+
+
+
+
+            IWebElement pesquisa = driver.FindElement(By.Name("q"));
+            pesquisa.SendKeys("Sensedia");
+
+            pesquisa.SendKeys(Keys.Enter);
+
+            IWebElement nome = driver.FindElement(By.XPath("//h3[contains(text(), 'Sensedia')]"));
+            Assert.Equal("Sensedia API Platform - Plataforma de APIs | Gerenc" +
+                "iamento ...", nome.Text);
+
+            ITakesScreenshot camera = driver as ITakesScreenshot;
+            Screenshot foto = camera.GetScreenshot();
+            foto.SaveAsFile(@"C:\\Users\\aline.oliveira\\source\\repos\\LearningQA\\LearningQA\\image.png", ScreenshotImageFormat.Png);
+            driver.Close();
+        }
+
+    }
+}
